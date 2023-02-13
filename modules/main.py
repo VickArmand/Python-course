428f3ca4ce5693a70c2e87cda59560cc0a64c80a
VickArmand <victormaina1962@gmail.com>
2023-02-13 10:49:49 +0300

    modules

--- modules/main.py
+++ modules/main.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 import modules
 # the above code is similar to from modules import add,sub
+arith =__import__('modules').add
+# this code means we are importing add function from modules
 print(modules.add(3,4))
 print(modules.sub(10,7))
+print(arith(2,6))
