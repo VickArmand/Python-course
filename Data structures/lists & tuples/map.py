f1703a9b8808a52ce4499a4a68a2995f049a35a3
VickArmand <victormaina1962@gmail.com>
2023-09-15 21:42:00 +0300

    map, lambda, comprehensions

--- Data structures/lists & tuples/map.py
+++ Data structures/lists & tuples/map.py
@@ -4,3 +4,10 @@
 # the second a sequence (e.g. a list) seq. 
 # map() applies the function func to all the elements of the sequence seq.
 squares = list(map(lambda x: x**2, range(10)))
+def fahrenheit(T):
+    return ((float(9)/5)*T + 32)
+def celsius(T):
+    return (float(5)/9)*(T-32)
+temperatures = (36.5, 37, 37.5, 38, 39)
+F = map(fahrenheit, temperatures)
+C = map(celsius, F)
