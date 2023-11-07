96682b9139ff254bfb6e3973786543efb776c272
VickArmand <victormaina1962@gmail.com>
2023-11-07 12:22:30 +0300

    roman to string correction

--- Data structures/sets and dictionaries/roman_to_string.py
+++ Data structures/sets and dictionaries/roman_to_string.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romantoint = {
-            'I': 1, 'IV': 4, 'V': 5, 
-            'IX': 9, 'X': 10, 'L': 50,
+            'I': 1, 'V': 5,
+            'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000}
     res = 0
     if roman_string in romantoint.keys():
         return romantoint[roman_string]
+    elif not isinstance(roman_string, str) or not roman_string:
+        return res
     else:
-        for i in roman_string:
-            if i in romantoint.keys():
-                res += romantoint[i]
+        i = 0
+        length = len(roman_string)
+        while i < length:
+            if i != length - 1:
+                if romantoint[roman_string[i]] < romantoint[
+                        roman_string[i + 1]]:
+                    res -= romantoint[roman_string[i]]
+                else:
+                    res += romantoint[roman_string[i]]
+            else:
+                res += romantoint[roman_string[i]]
+            i += 1
     return res
