69bda8697637face298390630c7c503cc781444d
VickArmand <victormaina1962@gmail.com>
2024-01-08 04:51:05 +0300

    get and post requests using urlllib

---
+++ Networking/using URLLIB/get-req.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode("utf-8")))
